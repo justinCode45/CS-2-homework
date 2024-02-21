210da746b42444e62f19794bd6e8d07db1208442
justin <justin.chen@thinktrace.com>
2024-02-21 05:47:54 +0000

    v1

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,2 @@
-sudo apt update
-# sudo apt install -y libgtk2.0-dev pkg-config
-sudo apt install ffmpeg libsm6 libxext6  -y
 python -m pip install --upgrade pip 
 pip install --user -r ./.devcontainer/requirements.txt
