fb8f6d393ed1adce007475fc2a57693603a8792a
justin <justin.chen@thinktrace.com>
2024-02-27 09:28:16 +0000

    ver 3.2

--- pro.py
+++
@@ -1,7 +0,0 @@
-import pstats
-from pstats import SortKey
-
-p = pstats.Stats('output.pstats')
-
-p.sort_stats(SortKey.CUMULATIVE).print_stats(30)
-p.sort_stats(SortKey.TIME).print_stats(30)
--- test.py
+++ test.py
@@ -1,23 +1,3 @@
-import threading
-import time
-
-def aa():
-    event.wait()            # 等待事件被觸發
-    event.clear()           # 觸發後將事件回歸原本狀態
-    for i in range(1,6):
-        print('A:',i)
-        time.sleep(0.5)
-
-def bb():
-    for i in range(10,60,10):
-        if i == 30:
-            event.set()     # 觸發事件
-        print('B:',i)
-        time.sleep(0.5)
-
-event = threading.Event()   # 註冊事件
-a = threading.Thread(target=aa)
-b = threading.Thread(target=bb)
-
-a.start()
-b.start()
+# for i in range(0.0,1.0,0.1):
+#     print (i)
+print( range(0.0,1.0,0.1))
