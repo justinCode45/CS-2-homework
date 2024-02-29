9881ebbe81f7c73fadcd92945428e898768bc5b7
justin <justin.chen@thinktrace.com>
2024-02-29 15:31:56 +0000

    HW2 ver0.5

--- HW2/HW2_112652044.py
+++ HW2/HW2_112652044.py
@@ -1,15 +1,39 @@
 import decimal
 from decimal import Decimal
-import matplotlib
-import numpy
+import matplotlib.pyplot as plt
+import numpy as np
 
-def montePi(c,n):
-    pass
+
+def montePi(n: int):
+
+    x = np.random.rand(n)
+    y = np.random.rand(n)
+    point = np.column_stack((x,y))
+    color_list = []
+    inside = np.sum(np.linalg.norm(point, axis=1) < 1).astype(int)
+    color_list = np.where(np.linalg.norm(point, axis=1) < 1, 'r', 'b')
+    fig, ax = plt.subplots()
+    pi = 4 * inside / n
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, 1)
+    ax.set_aspect('equal')
+    ax.add_patch(plt.Circle((0, 0), 1, fill=False, color='b'))
+    ax.scatter(x, y, c=color_list, s=1)
+    ax.set_title(f'Pi = {pi:.10f}')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+
+    plt.show()
+
+    return pi
+    
+
+    
 
 
 def main():
     
-
+    montePi(1000)
 
     pass
 
--- test.py
+++ test.py
@@ -1,3 +1,13 @@
-# for i in range(0.0,1.0,0.1):
-#     print (i)
-print( range(0.0,1.0,0.1))
+import matplotlib.animation as animation 
+import matplotlib.pyplot as plt   
+def animate(n):     
+    scatter = ax.scatter(n, n)     
+    if n == 0:         
+        return ax.get_children()[0],     
+    else:         
+        return ax.get_children()[:n]   
+fig, ax = plt.subplots() 
+ax.set_xlim(0, 20) 
+ax.set_ylim(0, 20)  
+ani = animation.FuncAnimation(fig, animate, interval=100, blit=True) 
+plt.show()
