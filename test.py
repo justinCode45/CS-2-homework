8d03a88e4e5089bdb383b7eb40bc679e4a5bc094
justin <justin.chen@thinktrace.com>
2024-03-30 08:48:49 +0000

    zip?

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-pass
+import zipfile
+
+targetfile = "zilQf4LTV1L7opzfsZkXhA11iLl3VD.zip"
+
+zfile = zipfile.ZipFile(targetfile)
+
+print(zfile.namelist())
+
+zfile.setpassword(b"123456")
+zfile.extractall()
+
+zfile.close()
