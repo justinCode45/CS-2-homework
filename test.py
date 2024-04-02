4ded6240727e379523a44cc0dc99fb8f28bc6c5b
justin <justin.chen@thinktrace.com>
2024-04-02 09:15:53 +0000

    HW5 ver1.1A

--- test.py
+++ test.py
@@ -1,4 +1,32 @@
 import turtle
+from urllib import request
+import csv
 
 
-print(turtle.font())
+def get_quake_data(year: int) -> list[dict]:
+    url = "https://earthquake.usgs.gov/fdsnws/event/1/query"
+    params = {
+        "format": "csv",
+        "starttime": f"{year}-01-01",
+        "endtime": f"{year}-01-02",
+        "minlatitude": 21.9267,
+        "maxlatitude": 24.9571,
+        "minlongitude": 119.8579,
+        "maxlongitude": 123.0428,
+        "minmagnitude": 4.5, 
+    }
+    response = request.urlopen(
+        url + "?" + "&".join([f"{k}={v}" for k, v in params.items()]))
+    reader = csv.reader(response.read().decode().splitlines())
+    header = next(reader)
+    quake_data = [dict(zip(header, row)) for row in reader]
+    return quake_data
+
+
+def main():
+    r = get_quake_data(2021)
+    print(r)
+
+
+if __name__ == "__main__":
+    main()
