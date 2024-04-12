e525c597293a887b5fe835cd4a1e3f929d107ae5
justin <justin.chen@thinktrace.com>
2024-04-12 11:35:49 +0000

    HW6

--- HW6/HW6_112652044.py
+++ HW6/HW6_112652044.py
@@ -2,22 +2,52 @@ from image import *
 
 
 def convolution(image:Image, kernel:list[list[float]]) -> Image:
+    print("Convolution")
+            
     result =EmptyImage(image.width, image.height)
-    for i in range(image.width):
-        for j in range(image.height):
-            sum = 0
-            for x in range(len(kernel)):
-                for y in range(len(kernel[0])):
-                    if i+x-len(kernel)//2 >= 0 and i+x-len(kernel)//2 < image.width and j+y-len(kernel[0])//2 >= 0 and j+y-len(kernel[0])//2 < image.height:
-                        sum += image[i+x-len(kernel)//2, j+y-len(kernel[0])//2] * kernel[x][y]
-            result[i,j] = sum
+    for row in range(image.width):
+        for col in range(image.height):
+           result.setPixel(row, col, image.getPixel(row, col))
+    for row in range(len(kernel)//2, image.width-len(kernel)//2):
+        for col in range(len(kernel)//2, image.height-len(kernel)//2):
+            red = 0
+            green = 0
+            blue = 0
+            for i in range(len(kernel)):
+                for j in range(len(kernel)):
+                    pixel = image.getPixel(row-len(kernel)//2+i, col-len(kernel)//2+j)
+                    red += pixel.red * kernel[i][j]
+                    green += pixel.green * kernel[i][j]
+                    blue += pixel.blue * kernel[i][j]
+            #normalize color
+            red = int(min(255, max(0, red)))
+            green = int(min(255, max(0, green)))
+            blue = int(min(255, max(0, blue)))
+            result.setPixel(row, col, Pixel(red, green, blue))
+            print(f"({row}, {col}) = ({red}, {green}, {blue})")
+    return result
+
+def resize(image:Image, width:int, height:int) -> Image:
+    result = EmptyImage(width, height)
+    for row in range(width):
+        for col in range(height):
+            x = row * image.width // width
+            y = col * image.height // height
+            result.setPixel(row, col, image.getPixel(x, y))
     return result
 
 def main():
-    image = Image("test.jpg")
+    image = Image("test.gif")
+    image = resize(image, image.width//2, image.height//2)
+    win = ImageWin(image.width*2, image.height)
     kernel = [[0, -1, 0], [-1, 5, -1], [0, -1, 0]]
+    burlk = [[1, 2, 1], [2, 1, 2], [1, 2, 1]]
+    # image = resize(image, image.width//2, image.height//2)
     result = convolution(image, kernel)
-    result.save("output.bmp")
+    image.draw(win)
+    result.setPosition(image.width, 0)
+    result.draw(win)
+    win.exitOnClick()
 
 if __name__ == "__main__":
     main()
