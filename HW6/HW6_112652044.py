7a08a04338705a6c68c45c47e51b48adcddf86f5
justin <justin.chen@thinktrace.com>
2024-04-13 02:12:40 +0000

    Refactor image mirroring and convolution functions

--- HW6/HW6_112652044.py
+++ HW6/HW6_112652044.py
@@ -1,13 +1,38 @@
 from image import *
 
 
-def convolution(image:Image, kernel:list[list[float]]) -> Image:
-    print("Convolution")
-            
-    result =EmptyImage(image.width, image.height)
+def mirrorX(image: Image) -> Image:
+    result = EmptyImage(image.width, image.height)
     for row in range(image.width):
         for col in range(image.height):
-           result.setPixel(row, col, image.getPixel(row, col))
+            result.setPixel(row, col, image.getPixel(row, image.height-col-1))
+    return result
+
+
+def mirrorY(image: Image) -> Image:
+    result = EmptyImage(image.width, image.height)
+    for row in range(image.width):
+        for col in range(image.height):
+            result.setPixel(row, col, image.getPixel(image.width-row-1, col))
+    return result
+
+
+
+def convolution(image: Image, kernel: list[list[float]]) -> Image:
+    # normalize kernel
+    sum = 0
+    for i in range(len(kernel)):
+        for j in range(len(kernel)):
+            sum += kernel[i][j]
+    if sum != 0:
+        for i in range(len(kernel)):
+            for j in range(len(kernel)):
+                kernel[i][j] /= sum
+
+    result = EmptyImage(image.width, image.height)
+    for row in range(image.width):
+        for col in range(image.height):
+            result.setPixel(row, col, image.getPixel(row, col))
     for row in range(len(kernel)//2, image.width-len(kernel)//2):
         for col in range(len(kernel)//2, image.height-len(kernel)//2):
             red = 0
@@ -15,19 +40,20 @@ def convolution(image:Image, kernel:list[list[float]]) -> Image:
             blue = 0
             for i in range(len(kernel)):
                 for j in range(len(kernel)):
-                    pixel = image.getPixel(row-len(kernel)//2+i, col-len(kernel)//2+j)
+                    pixel = image.getPixel(
+                        row-len(kernel)//2+i, col-len(kernel)//2+j)
                     red += pixel.red * kernel[i][j]
                     green += pixel.green * kernel[i][j]
                     blue += pixel.blue * kernel[i][j]
-            #normalize color
+            # make sure the value is in the range of 0-255
             red = int(min(255, max(0, red)))
             green = int(min(255, max(0, green)))
             blue = int(min(255, max(0, blue)))
             result.setPixel(row, col, Pixel(red, green, blue))
-            print(f"({row}, {col}) = ({red}, {green}, {blue})")
     return result
 
-def resize(image:Image, width:int, height:int) -> Image:
+
+def resize(image: Image, width: int, height: int) -> Image:
     result = EmptyImage(width, height)
     for row in range(width):
         for col in range(height):
@@ -36,18 +62,31 @@ def resize(image:Image, width:int, height:int) -> Image:
             result.setPixel(row, col, image.getPixel(x, y))
     return result
 
+
 def main():
-    image = Image("test.gif")
-    image = resize(image, image.width//2, image.height//2)
-    win = ImageWin(image.width*2, image.height)
-    kernel = [[0, -1, 0], [-1, 5, -1], [0, -1, 0]]
-    burlk = [[1, 2, 1], [2, 1, 2], [1, 2, 1]]
-    # image = resize(image, image.width//2, image.height//2)
-    result = convolution(image, kernel)
-    image.draw(win)
-    result.setPosition(image.width, 0)
-    result.draw(win)
-    win.exitOnClick()
+    filename = input("Enter the image file name: ")
+    try:
+        image = Image(filename)
+    except FileNotFoundError:
+        print("File not found.")
+        return
+    image = resize(image, image.width//3, image.height//3)
+    commond = input("Enter the command (v,b,h,s): ")
+    for c in commond:
+        win = ImageWin(image.width, image.height, "Image")
+        if c == 'v':
+            result = mirrorX(image)
+        elif c == 'h':
+            result = mirrorY(image)
+        elif c == 'b':
+            kernel = [[1, 2, 1], [2, 1, 2], [1, 2, 1]]
+            result = convolution(image, kernel)
+        elif c == 's':
+            kernel = [[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]]
+            result = convolution(image, kernel)
+        result.draw(win)
+        win.exit_on_click()
+
 
 if __name__ == "__main__":
     main()
