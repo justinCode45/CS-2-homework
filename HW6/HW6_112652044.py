123b30ca1f68603005795eca0fa762e4a7509952
justin <justin.chen@thinktrace.com>
2024-04-13 09:00:46 +0000

    .

--- HW6/HW6_112652044.py
+++ HW6/HW6_112652044.py
@@ -1,21 +1,24 @@
 from image import *
 
 
+brulKernel = [[1, 2, 1], [2, 1, 2], [1, 2, 1]]
+sharpKernel = [[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]]
+
+
 def mirrorX(image: Image) -> Image:
     result = EmptyImage(image.width, image.height)
-    for row in range(image.width):
-        for col in range(image.height):
-            result.setPixel(row, col, image.getPixel(row, image.height-col-1))
+    for row in range(image.height):
+        for col in range(image.width):
+            result.setPixel(col, row, image.getPixel(image.width-col-1, row))
     return result
 
 
 def mirrorY(image: Image) -> Image:
     result = EmptyImage(image.width, image.height)
-    for row in range(image.width):
-        for col in range(image.height):
-            result.setPixel(row, col, image.getPixel(image.width-row-1, col))
+    for row in range(image.height):
+        for col in range(image.width):
+            result.setPixel(col, row, image.getPixel(col, image.height-row-1))
     return result
-
 
 
 def convolution(image: Image, kernel: list[list[float]]) -> Image:
@@ -30,18 +33,18 @@ def convolution(image: Image, kernel: list[list[float]]) -> Image:
                 kernel[i][j] /= sum
 
     result = EmptyImage(image.width, image.height)
-    for row in range(image.width):
-        for col in range(image.height):
-            result.setPixel(row, col, image.getPixel(row, col))
-    for row in range(len(kernel)//2, image.width-len(kernel)//2):
-        for col in range(len(kernel)//2, image.height-len(kernel)//2):
+    for row in range(image.height):
+        for col in range(image.width):
+            result.setPixel(col, row, image.getPixel(col, row))
+    for row in range(len(kernel)//2, image.height-len(kernel)//2):
+        for col in range(len(kernel)//2, image.width-len(kernel)//2):
             red = 0
             green = 0
             blue = 0
             for i in range(len(kernel)):
                 for j in range(len(kernel)):
                     pixel = image.getPixel(
-                        row-len(kernel)//2+i, col-len(kernel)//2+j)
+                        col-len(kernel)//2+j, row-len(kernel)//2+i)
                     red += pixel.red * kernel[i][j]
                     green += pixel.green * kernel[i][j]
                     blue += pixel.blue * kernel[i][j]
@@ -49,21 +52,22 @@ def convolution(image: Image, kernel: list[list[float]]) -> Image:
             red = int(min(255, max(0, red)))
             green = int(min(255, max(0, green)))
             blue = int(min(255, max(0, blue)))
-            result.setPixel(row, col, Pixel(red, green, blue))
+            result.setPixel(col, row, Pixel(red, green, blue))
     return result
 
 
 def resize(image: Image, width: int, height: int) -> Image:
     result = EmptyImage(width, height)
-    for row in range(width):
-        for col in range(height):
-            x = row * image.width // width
-            y = col * image.height // height
-            result.setPixel(row, col, image.getPixel(x, y))
+    for row in range(height):
+        for col in range(width):
+            y = row * image.width // width
+            x = col * image.height // height
+            result.setPixel(col, row, image.getPixel(x, y))
     return result
 
 
 def main():
+
     filename = input("Enter the image file name: ")
     try:
         image = Image(filename)
@@ -72,19 +76,22 @@ def main():
         return
     image = resize(image, image.width//3, image.height//3)
     commond = input("Enter the command (v,b,h,s): ")
+
     for c in commond:
+
         win = ImageWin(image.width, image.height, "Image")
+
         if c == 'v':
             result = mirrorX(image)
         elif c == 'h':
             result = mirrorY(image)
         elif c == 'b':
-            kernel = [[1, 2, 1], [2, 1, 2], [1, 2, 1]]
-            result = convolution(image, kernel)
+            result = convolution(image, brulKernel)
         elif c == 's':
-            kernel = [[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]]
-            result = convolution(image, kernel)
+            result = convolution(image, sharpKernel)
+
         result.draw(win)
+
         win.exit_on_click()
 
 
